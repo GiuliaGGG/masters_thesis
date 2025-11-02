00a4febb99ae52d7746f6ee86b0ca2ea9a1f002b
GiuliaGGG <144107205+GiuliaGGG@users.noreply.github.com>
2025-11-02 17:45:17 +0100

    trying the slimmest possible version of the data that runs

--- synth.r
+++ synth.r
@@ -4,6 +4,7 @@
 library("Synth")
 library(readr)
 library('dplyr')
+library(skimr)
 
 
 df <- read_csv("data/merged_dataset_2025-11-01_16-55.csv")
@@ -14,11 +15,22 @@ id_cols <- c("boycotted", "ticker", "fp", "end")
 # Step 2: preprocessing
 df_clean <- df %>%
   filter(ticker != "COKE") %>% #make sure there just one target
+  filter(ticker != "CMG") %>% #    drop for now, must see if script runs when df is balanced 
+  filter(ticker != "WEN") %>% #drop for now, must see if script runs when df is balanced 
+  filter(ticker != "BROS") %>% #drop for now, must see if script runs when df is balanced 
   filter(fy != "2026") %>% 
   mutate(company_id = as.numeric(as.factor(ticker))) %>%  
+  group_by(ticker, fy) %>%
+  # count quarters per company-year
+  mutate(n_quarters = n_distinct(fp)) %>%
+  # keep only company-years with at least 3 quarters
+  filter(n_quarters >= 3) %>%
+  ungroup() %>%
+  select(-n_quarters) %>%       # remove helper column
   mutate(across(
     .cols = -all_of(id_cols),          # all columns except identifiers
     .fns  = ~ as.numeric(.))) %>%      # convert to numeric, quietly 
+  ungroup() %>%  
   mutate(
     # Turn fp (Q1, Q2, Q3, Q4) into numeric quarter fraction
     quarter_num = case_when(
@@ -32,7 +44,20 @@ df_clean <- df %>%
     time_numeric = fy + quarter_num) %>% 
   as.data.frame()
  
-  
+'FOR NOW drop nas for the dependent variable'
+df_clean <- df_clean %>%
+  filter(!is.na(revenue))
+
+
+df_clean <- df_clean %>%
+  group_by(ticker) %>%
+  arrange(time_numeric) %>%
+  mutate(
+    revenue = na.approx(revenue, na.rm = FALSE)
+  ) %>%
+  ungroup()
+
+
 
 ###################################################
 ### chunk number 2: 
@@ -43,18 +68,19 @@ df_clean[85:89, 1:4]
 ### chunk number 3: 
 ###################################################
 
+
 dataprep(
   foo = df_clean,
-  predictors = c("assets", "employees",  "net_margin_pct",
-                 "r_and_d", "long_term_debt", "operating_expenses"),
+  predictors = c("assets",  "net_margin_pct"),
   dependent = "revenue",
   unit.variable = "company_id",          # numeric ID column
   unit.names.variable = "ticker",        # readable name
   time.variable = "time_numeric",                  # or numeric time index
-  treatment.identifier = 5,              # treated company ID
-  controls.identifier = c(1,2,3,4,6),         # control company IDs
-  time.optimize.ssr = 2006:2021,         # pre-treatment period
-  time.plot = 2006:2025                  # full period to plot
+  treatment.identifier = 3,              # treated company ID
+  controls.identifier = c(1,2), # control company IDs
+  time.predictors.prior = 2010.25:2023.75,
+  time.optimize.ssr = 2010.25:2023.75,         # pre-treatment period
+  time.plot = 2010.25:2025.75                  # full period to plot
 )
 
 
