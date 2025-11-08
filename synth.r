2c0e42056c53c4b752bf93101c7346c543d8eb9f
GiuliaGGG <144107205+GiuliaGGG@users.noreply.github.com>
2025-11-08 19:45:09 +0100

    plotting

--- synth.r
+++ synth.r
@@ -6,6 +6,8 @@ library(readr)
 library(dplyr)
 library(skimr)
 library(tidyr)
+library(ggplot2)
+
 
 df <- read_csv("data/merged_dataset_2025-11-01_16-55.csv")
 
@@ -14,7 +16,7 @@ df_clean <- df %>%
   
   # Filter
     # rows
-    filter(ticker != "COKE") %>% #make sure there just one target
+    filter(ticker != "SBUX") %>% #make sure there just one target
     filter(ticker != "BROS") %>%  #drop for now, has very few years , might be an issue from the data fetching 
     filter(fy < "2026") %>% 
     filter(fy != "2009") %>% # quick and dirty fix for now
@@ -23,7 +25,8 @@ df_clean <- df %>%
     select(where(~ !all(is.na(.)))) %>% 
   
   # Create useful columns 
-    mutate(company_id = as.numeric(as.factor(ticker))) %>%  
+    mutate(company_id = as.numeric(as.factor(ticker))) %>%
+    mutate(net_income_margin = net_income / assets) %>% 
   
     # Balance 
     #complete(
@@ -64,7 +67,7 @@ df_clean <- df %>%
 
     #inpute nas for the dependent variable 
     group_by(ticker) %>% 
-    fill(net_income, .direction = "downup") %>% 
+    fill(net_income_margin, .direction = "downup") %>% 
     ungroup() %>% 
     
   
@@ -86,19 +89,79 @@ df_clean <- df %>%
     as.data.frame()  
   
   
-  
-  
+
 # dataprep 
-dataprep(
+dataprep.out <- dataprep(
   foo = df_clean,
-  predictors = c("assets",  "net_margin_pct"),
-  dependent = "net_income",
+  predictors = c("assets", 'liabilities_current', 'shares_basic', 'revenue'),
+  dependent = "net_income_margin",
   unit.variable = "company_id",          # numeric ID column
   unit.names.variable = "ticker",        # readable name
   time.variable = "time_numeric",                  # or numeric time index
-  treatment.identifier = 1,              # treated company ID
-  controls.identifier = c(2:5), # control company IDs
+  treatment.identifier = 2,              # treated company ID
+  controls.identifier = c(1,4,3,5), # control company IDs
   time.predictors.prior = 2010.25:2023.75,
   time.optimize.ssr = 2010.25:2023.75,         # pre-treatment period
   time.plot = 2010.25:2025.75                  # full period to plot
 )
+
+###################################################
+### chunk number 4: 
+###################################################
+dataprep.out$X1 #treated unit’s predictor values (Basque Country).
+
+###################################################
+### chunk number 5: 
+###################################################
+dataprep.out$Z1 #treated unit’s outcome values (Basque Country).
+
+###################################################
+### chunk number 7: 
+###################################################
+# run synth
+synth.out <- synth(
+  data.prep.obj = dataprep.out,
+  method = "BFGS"
+)
+
+###################################################
+### chunk number 8: 
+###################################################
+gaps <- dataprep.out$Y1plot - (dataprep.out$Y0plot %*% synth.out$solution.w)
+gaps[1:3, 1]
+
+###################################################
+### chunk number 9: 
+###################################################
+synth.tables <- synth.tab(dataprep.res = dataprep.out,
+                          synth.res = synth.out
+)
+
+###################################################
+### chunk number 10: 
+###################################################
+names(synth.tables)
+
+###################################################
+### chunk number 11: 
+###################################################
+synth.tables$tab.pred
+
+###################################################
+### chunk number 12: 
+###################################################
+synth.tables$tab.w[8:14, ]
+
+
+###################################################
+### chunk number 13: 
+###################################################
+path.plot(synth.res = synth.out,
+          dataprep.res = dataprep.out,
+          Ylab = "net_income_margin",
+          Xlab = "year",
+          Ylim = c(0,0.4),
+          Legend = c("coke","Synthetic coke"),
+          Legend.position = "topright"
+)
+
